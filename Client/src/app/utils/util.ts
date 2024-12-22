9afbd53ea9313bf33edef365c37a5c9dd48a6c94
mohit kala jain <mohitkala0@gmail.com>
2024-12-22 22:07:16 +0530

    basket summary

--- Client/src/app/utils/util.ts
+++ Client/src/app/utils/util.ts
@@ -2,3 +2,7 @@ export function getCookie(key:string){
     const buyerId=document.cookie.match(`(^|;)\\s*${key}\\s*=\\s*([^;]+)`);
     return buyerId?buyerId.pop():"";
 }
+
+export function currencyFormat(amount:number){
+    return `₹${amount.toFixed(2)}`
+}
